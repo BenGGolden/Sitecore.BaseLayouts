a40af2116c129c3c8ed6e92ef2cd9948ddd214e5
Ben Golden <bgolden@awareweb.com>
2015-11-16 00:25:19 -0600

    Add field validator to prevent creation of a circular reference in the base layout field

---
+++ Sitecore.BaseLayouts/sitecore modules/shell/BaseLayouts/Data/BaseLayoutCircularReferenceValidator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Sitecore.Data;
+using Sitecore.Data.Validators;
+using Sitecore.Diagnostics;
+
+namespace Sitecore.BaseLayouts.Data
+{
+    public class BaseLayoutCircularReferenceValidator : StandardValidator
+    {
+        private readonly IBaseLayoutValidator _baseLayoutValidator;
+
+        public BaseLayoutCircularReferenceValidator() : this(new BaseLayoutValidator())
+        {
+        }
+
+        public BaseLayoutCircularReferenceValidator(IBaseLayoutValidator baseLayoutValidator)
+        {
+            Assert.ArgumentNotNull(baseLayoutValidator, "baseLayoutValidator");
+            _baseLayoutValidator = baseLayoutValidator;
+        }
+
+        protected override ValidatorResult Evaluate()
+        {
+            var value = GetControlValidationValue();
+            if (string.IsNullOrEmpty(value))
+            {
+                return ValidatorResult.Valid;
+            }
+
+            ID id;
+            if (!ID.TryParse(value, out id))
+            {
+                return GetFailedResult(ValidatorResult.Error);
+            }
+
+            var item = GetItem();
+            var baseLayout = item.Database.GetItem(id);
+            if (baseLayout == null)
+            {
+                return GetFailedResult(ValidatorResult.Error);
+            }
+
+            if (_baseLayoutValidator.CreatesCircularBaseLayoutReference(item, baseLayout))
+            {
+                return GetFailedResult(ValidatorResult.FatalError);
+            }
+
+            return ValidatorResult.Valid;
+        }
+
+        protected override ValidatorResult GetMaxValidatorResult()
+        {
+            return GetFailedResult(ValidatorResult.FatalError);
+        }
+
+        public override string Name
+        {
+            get { return "Base Layout Circular Reference Validator"; }
+        }
+    }
+}
